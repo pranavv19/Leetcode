1486b9013a76ba84e9e0a410b63fcdfdf3ceed2f
Pranav Gupta <96830638+pranavv19@users.noreply.github.com>
2024-07-31 01:04:42 +0530

    Time: 9 ms (36.05%) | Memory: 45.2 MB (35.88%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -3,25 +3,15 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
         int cnt=0;
-        for(int i=0,j=0;i<g.length;i++)
-        {
-            if(j<s.length)
-            {
-                if(g[i]<=s[j])
-                {
-                    g[i]-=s[j];
-                    
-                    cnt++;
-                }
-                else 
-                i--;
-                j++;
-            }
-        }
-        
-        // for(int i=0;i<g.length;i++)
-        // if(g[i]==0)
-        // cnt++;
+       int i = 0, j = 0;
+while (i < g.length && j < s.length) {
+    if (g[i] <= s[j]) {
+        g[i] -= s[j];
+        cnt++;
+        i++;
+    }
+    j++;
+}
         return cnt;
         
     }
